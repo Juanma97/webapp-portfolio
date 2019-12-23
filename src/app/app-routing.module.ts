9d347380c2e44396843f1af2b0e53ddde78cf4a1
Juan Manuel Perez Toro <juanma97@outlook.com>
2019-12-23 22:30:50 +0100

    About component created, and routing, and separate components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
+import {AboutComponent} from '../views/about/about.component';
+import {HomeComponent} from '../views/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { AboutComponent } from '../views/about/about.component';
+import { HomeComponent } from '../views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
